0dc40e1ad8d69ac52782de6d112d322dc30d2e5d
seinarukiro <seinarukiro@seinarukirosMBP.fritz.box>
2024-09-12 00:20:30 +0200

    chore: Update views.py to use async/await for database operations

--- not_to_pixel/generator/views.py
+++ not_to_pixel/generator/views.py
@@ -5,62 +5,57 @@ from django.utils import timezone
 from .models import User, Pictures
 
 class CreateUserAPIView(APIView):
-    def post(self, request):
+    async def post(self, request):
         telegram_id = request.data.get('telegram_id')
         if not telegram_id:
             return Response({"error": "telegram_id is required"}, status=status.HTTP_400_BAD_REQUEST)
 
-        user, created = User.objects.get_or_create(telegram_id=telegram_id)
+        user, created = await User.objects.aget_or_create(telegram_id=telegram_id)
         if created:
             return Response({"message": "User created", "telegram_id": user.telegram_id}, status=status.HTTP_201_CREATED)
         return Response({"message": "User already exists", "telegram_id": user.telegram_id}, status=status.HTTP_200_OK)
 
 
 class UpdateLastRequestAPIView(APIView):
-    def post(self, request, telegram_id):
+    async def post(self, request, telegram_id):
         try:
-            user = User.objects.get(telegram_id=telegram_id)
-            user.last_request = timezone.now()  #Обновляю время последнего запроса
-            user.save()
+            user = await User.objects.aget(telegram_id=telegram_id)
+            user.last_request = timezone.now()
+            await user.asave()
             return Response({"message": "Last request time updated"}, status=status.HTTP_200_OK)
         except User.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
 
 class CreatePictureAPIView(APIView):
-    def post(self, request, user_id):
+    async def post(self, request, user_id):
         try:
-            user = User.objects.get(id=user_id)
+            user = await User.objects.aget(id=user_id)
         except User.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        data = request.data.get('data')  #Получаею жсон данные
+        data = request.data.get('data')
         if not data:
             return Response({"error": "Data field is required"}, status=status.HTTP_400_BAD_REQUEST)
 
-        picture = Pictures.objects.create(user=user, data=data)
+        picture = await Pictures.objects.acreate(user=user, data=data)
         return Response({
             "message": "Picture created",
             "picture_id": picture.id,
             "data": picture.data
         }, status=status.HTTP_201_CREATED)
-    
+
 
 class ListUserPicturesAPIView(APIView):
-    def get(self, request, telegram_id):
+    async def get(self, request, telegram_id):
         try:
-            user = User.objects.get(telegram_id=telegram_id)
+            user = await User.objects.aget(telegram_id=telegram_id)
         except User.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        # Получаем все картинки пользователя
-        pictures = Pictures.objects.filter(user=user)
-        pictures_data = [
-            {
-                "picture_id": picture.id,
-                "data": picture.data,
-            } for picture in pictures
-        ]
+
+        pictures = await Pictures.objects.filter(user=user).avalues('id', 'data')
+        pictures_data = list(pictures)  # Преобразую кверисет в список
 
         return Response({
             "user": user.telegram_id,
